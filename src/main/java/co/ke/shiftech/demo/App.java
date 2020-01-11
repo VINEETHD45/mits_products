8d0088a50eb83d9a13d84cb6a6de064ccb86477e
dev-techguy <dev.techguy@gmail.com>
2020-01-12 02:47:35 +0300

    first commit

---
+++ src/main/java/co/ke/shiftech/demo/App.java
@@ -0,0 +1,13 @@
+package co.ke.shiftech.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    // Run the application here
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
---
+++ src/test/java/co/ke/shiftech/demo/AppTests.java
@@ -0,0 +1,13 @@
+package co.ke.shiftech.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
